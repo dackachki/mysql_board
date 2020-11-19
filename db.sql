721ea879576d61532a29cf10598967b5bb886c9b
dackachki <skatmddnjs77@gmail.com>
2020-11-19 22:14:37 +0900

    sql 추가

--- db.sql
+++ db.sql
@@ -38,4 +38,20 @@ title = '제목3',
 `body` = '내용3',
 memberId = 1,
 boardId = 1;
+// -------------------------------------------------------------------------------
+create table recommand (
+     articleNum int(10) not null,
+     memberId int(10) not null
+);
+insert into recommand set articleNum = 1,memberId = 4;
+INSERT INTO recommand SET articleNum = 2,memberId = 2;
+INSERT INTO recommand SET articleNum = 3,memberId = 3;
+INSERT INTO recommand SET articleNum = 1,memberId = 1;
 
+select a.id as '게시물번호',
+group_concat(r.memberId) as '추천인',
+count(r.memberId) as '추천수'
+from recommand as r
+join article as a
+on a.id = r.articleNum
+group by a.id;
